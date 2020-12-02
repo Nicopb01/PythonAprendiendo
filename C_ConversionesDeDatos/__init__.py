d0b5eb91cd5333fe2ed732fa3cbd1cf201f67ab3
Nicopb01 <57004894+Nicopb01@users.noreply.github.com>
2020-12-02 01:50:08 +0100

    Add files via upload

---
+++ C_ConversionesDeDatos/__init__.py
@@ -0,0 +1,13 @@
+numero1=36482
+cadena=str(numero1)#asi transformamos un int en un string
+print(cadena)
+print(type(cadena))
+
+
+cadena2='25'
+numero2=int(cadena2)
+print(cadena2)
+print(type(numero2))
+
+suma=cadena2+cadena
+print(suma)
