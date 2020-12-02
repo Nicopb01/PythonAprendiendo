e33d7d826a5a25c9345bdc8b2f1a9ef8cda1c283
Nicopb01 <57004894+Nicopb01@users.noreply.github.com>
2020-12-02 01:48:40 +0100

    Add files via upload

---
+++ CursoPython1/A_Variables/__init__.py
@@ -0,0 +1,11 @@
+cadena1="Hola"
+cadena2=" Mundo"
+cadena3= cadena1 + cadena2
+print (cadena3)
+print (type (cadena3)) #me imprime el tipo de dato que es
+
+
+a=3764238746
+b=862893652
+producto=a*b
+print(producto)
